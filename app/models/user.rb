2e9d4cbdf5e71c1468a5377c82b8791eaac1c0b4
luyanda <luyandadladla92@gmail.com>
2024-02-01 13:08:50 +0200

    initial push

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  extend FriendlyId
+  friendly_id :username, use: :slugged
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         has_many :posts, dependent: :destroy
+
+         def full_name
+          "#{first_name.capitalize unless first_name.nil?} #{last_name.capitalize unless last_name.nil?}"
+         end
+end
